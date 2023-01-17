702bac6fd1fa5d91c14ac5d5af1298fde0805377
Alexandre SOARES <72209227+AS-37@users.noreply.github.com>
2023-01-17 11:55:00 +0100

    Update FaceDetection.cs

--- src/Alexandre.Soares.FaceDetection/FaceDetection.cs
+++ src/Alexandre.Soares.FaceDetection/FaceDetection.cs
@@ -1,11 +1,11 @@
 ﻿using OpenCvSharp;
 namespace Alexandre.Soares.FaceDetection;
 
-public class FaceDetection {
-    public IList<ObjectDetectionPoint> DetectInScenes(IList<byte[]>
-        imagesSceneData)
+public class FaceDetection
+{
+    public IList<ObjectDetectionPoint> DetectInScenes(IList<byte[]> imagesSceneData)
     {
-// TODO implement your code here
-        throw new NotImplementedException();
+        return null;
     }
+    
 }
